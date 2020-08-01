7dda0a03bca571aedd931a38776c525a3d7bc840
Tatiana <tatianaw83@gmail.com>
2020-08-01 12:10:05 -0700

    first commit

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette ">
+                {/* header */}
+                <div className="Palette-colors">
+                    {}
+                </div>
+                {/* footer */}
+            </div>
+        )
+    }
+}
